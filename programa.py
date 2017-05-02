6ad28789c99f9ee8fd3c1647596d658fef94b512
AdanReina2 <adan.reina020513@gmail.com>
2017-05-02 11:54:03 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -9,17 +9,7 @@ def consulta():
 
 @post('/resultado')
 def resultado():
-    city = request.forms.get('city')
-    cate = request.forms.get('cate')
-    url_base = "http://api.eventful.com/rest"
-    k=open("key.txt","r")
-    key = k.readline()
-    k.close()
-    payload = {'app_key':key, 'keywords':cate,'location':city,'date':'Future'}
-    r = requests.get(url_base + '/events/search', params=payload)
-    if r.status_code == 200:
-        doc = etree.fromstring(r.text.encode('utf-8'))
-        return template("resultado.tpl",doc=doc,city=city,cate=cate)
+    print "Hola"
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
