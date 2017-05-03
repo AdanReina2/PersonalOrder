cf3132941c0644ed96735c54b1c7c1de96bc8e54
AdanReina2 <adan.reina020513@gmail.com>
2017-05-03 10:20:44 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -11,8 +11,4 @@ def consulta():
 def resultado():
     print "Hola"
 
-@route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='static')
-
 run(host='0.0.0.0',port=argv[1])
