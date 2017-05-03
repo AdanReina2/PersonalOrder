5a30c3ccbe34a8d4a6d4d051a1e2e1e5ec707ed3
AdanReina2 <adan.reina020513@gmail.com>
2017-05-03 14:03:28 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -71,7 +71,7 @@ def listareventos():
     if token_valido():
         token = request.get_cookie("token", secret='some-secret-key')
         oauth2 = OAuth2Session(client_id, token=token)
-        r = oauth2.get('https://www.googleapis.com/calendar/v3/calendars')
+        r = oauth2.calendar.events.list('https://www.googleapis.com/calendar/v3/calendars')
         doc = json.loads(r.content)
         return doc
     return template('listareventos.tpl')
