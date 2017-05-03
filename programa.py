6ff070063210f088b340fe98eaae0487daf155e4
AdanReina2 <adan.reina020513@gmail.com>
2017-05-03 10:59:28 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -2,11 +2,6 @@ from bottle import get, post, route, run, template
 from lxml import etree
 import requests
 from sys import argv
-from oauth2client.service_account import ServiceAccountCredentials
-
-scopes = ['https://www.googleapis.com/auth/sqlservice.admin']
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name('key.json', scopes=scopes)
 
 @route('/')
 def consulta():
