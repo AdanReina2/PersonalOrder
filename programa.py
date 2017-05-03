e02b2ae35874f11c1f008382015086d21de278c6
AdanReina2 <adan.reina020513@gmail.com>
2017-05-03 17:17:10 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -73,8 +73,7 @@ def listareventos():
         oauth2 = OAuth2Session(client_id, token=token)
         r = oauth2.get('https://www.googleapis.com/calendar/v3/users/me/calendarList')
         doc = json.loads(r.content)
-        doc2 = doc["summary"]
-    return template('listareventos.tpl',doc2=doc2)
+    return template('listareventos.tpl',doc=doc)
 
 @route('/nuevoevento')
 def nuevoevento():
