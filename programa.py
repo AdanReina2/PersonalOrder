995d5816c488f98a1b4d7e7b393fcab40b9c293c
Adan Reina <adan@Hubble>
2017-05-16 10:45:26 +0200

    modificamos cosas

--- programa.py
+++ programa.py
@@ -105,7 +105,7 @@ def listareventos():
         idcal = request.forms.get('calendario')
         token = request.get_cookie("token", secret='some-secret-key')
         oauth2 = OAuth2Session(client_id, token=token)
-        url_base = 'https://www.googleapis.com/calendar/v3/calendars/'+calendario+'/events'
+        url_base = 'https://www.googleapis.com/calendar/v3/calendars/'+idcal+'/events'
         payload = {'key':key}
         r3 = oauth2.get(url_base,params=payload)
         doc = json.loads(r3.content)
