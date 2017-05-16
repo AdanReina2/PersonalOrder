0977615e6910b955468258e9b7af80791bcb0956
Adan Reina <adan@Hubble>
2017-05-16 10:29:42 +0200

    modificamos cosas

--- programa.py
+++ programa.py
@@ -111,7 +111,7 @@ def listareventos():
         for i in doc["items"]:
             lista.append(i["summary"])
             if "location" in i: 
-                lista2.append("Este evento no tiene localización")
+                lista2.append("null")
         return template('listareventos.tpl',lista=lista,lista2=lista2,login=token_valido())
 
 @route('/listareventos2',method='post')
