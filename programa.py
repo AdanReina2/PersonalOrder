82f6bc3a119023805a94ddd52181b24052703ba9
Adan Reina <adan@Hubble>
2017-05-22 10:52:38 +0200

    modificamos cosas

--- programa.py
+++ programa.py
@@ -350,9 +350,8 @@ def formulariomapa(posicion):
         nuevaposicion = request.forms.get('nuevaposicion')
         token = request.get_cookie("token", secret='some-secret-key')
         oauth2 = OAuth2Session(client_id, token=token)
-        url_base = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+posicion+'&destinations='+nuevaposicion
-        payload = {'key':key}
-        r11 = requests.forms.get(url_base,params=payload)
+        url_base = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+posicion+'&destinations='+nuevaposicion+key
+        r11 = requests.forms.get(url_base)
         r12 = requests.get(r11)
         a = r12.text["rows"]
         b = a[0].get("elements")
